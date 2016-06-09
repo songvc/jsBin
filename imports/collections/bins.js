426beab999c64125a8f0edf39e1844c7b96bdac3
Vincent Song <songvc@users.noreply.github.com>
2016-06-08 18:21:42 -0700

    jsbin markdown convertor

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -11,6 +11,9 @@ Meteor.methods({
   },
   'bins.remove': function (bin) {
     return Bins.remove(bin);
+  },
+  'bins.update': function(bin, newContent) {
+    return Bins.update(bin._id, { $set: { content: newContent}});
   }
 
 });
